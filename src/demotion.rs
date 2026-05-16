1c513ccb33feb33931d68765fc93a806a97e8475
Brad Cannon <61765732+ForeverAngry@users.noreply.github.com>
2026-05-16 11:50:13 -0400

    feat: add observe hooks for memory lifecycle

--- src/demotion.rs
+++ src/demotion.rs
@@ -187,7 +187,16 @@ impl DemotionHook for MemvidDemotionHook {
             for msg in &messages {
                 self.write(conversation_id, msg)?;
             }
-            commit_if_each_turn(&self.store, &self.config)
+            commit_if_each_turn(&self.store, &self.config)?;
+            #[cfg(feature = "observe")]
+            rig_observe::emit_kind(
+                conversation_id,
+                rig_observe::EventKind::MemoryDemoted {
+                    demoted_count: messages.len(),
+                    tags: self.config.default_tags.clone(),
+                },
+            );
+            Ok(())
         })
     }
 }
